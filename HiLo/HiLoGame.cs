6f02a9804e79642e63b1737210a97606ea7f053d
r <galkin.2020@CLOUD-RDHOST1>
2023-02-10 12:06:20 +0700

    Добавьте файлы проекта.

---
+++ HiLo/HiLoGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HiLo
+{
+
+
+    static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int nextNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        public static int GetPot() { return pot; }
+
+        public static void Guess(bool higher)
+        {
+            if ((higher && nextNumber >= currentNumber) ||
+                    (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong.");
+                pot--;
+            }
+            currentNumber = nextNumber;
+            nextNumber = random.Next(1, MAXIMUM + 1);
+            Console.WriteLine($"The current number is {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (nextNumber >= half)
+                Console.WriteLine($"The current number is {currentNumber}, the next number is at least {half}");
+            else Console.WriteLine($"The current number is {currentNumber}, the next is at most {half}");
+            pot--;
+        }
+    }
+
+}
